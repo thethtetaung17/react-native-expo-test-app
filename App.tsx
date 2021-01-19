b958af0a9bab6372fcb7afc5dd5f855f9f91333e
Thet Htet Aung <thethtetaung@Thets-MacBook-Pro.local>
2021-01-19 17:26:59 +0630

    File structure re-arrange and use redux+hooks

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-import store from './redux/store';
+import useCachedResources from './src/hooks/useCachedResources';
+import useColorScheme from './src/hooks/useColorScheme';
+import Navigation from './src/navigation';
+import store from './src/redux/store';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
